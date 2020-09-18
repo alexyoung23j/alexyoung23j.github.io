e90a6aec18b7ff312c8585e923422fe6a1fa16cc
Alex Young <alexyoung23j@gmail.com>
2020-09-17 17:01:00 -0700

    Updates

---
+++ precache-manifest.ddd674cd3b194b63fa34b2c5c0357208.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "780485ff3c2a5eb79d7b5e192d6960d6",
+    "url": "/alexyoung23j.github.io/index.html"
+  },
+  {
+    "revision": "878e7e041b510dad76a4",
+    "url": "/alexyoung23j.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ac0c04300092e7871485",
+    "url": "/alexyoung23j.github.io/static/js/2.8e41c90a.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/alexyoung23j.github.io/static/js/2.8e41c90a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "878e7e041b510dad76a4",
+    "url": "/alexyoung23j.github.io/static/js/main.4600f4e6.chunk.js"
+  },
+  {
+    "revision": "4dfe31541fad98877d53",
+    "url": "/alexyoung23j.github.io/static/js/runtime-main.12127be7.js"
+  },
+  {
+    "revision": "ed6b64c97197f180c77bc6bbeba66338",
+    "url": "/alexyoung23j.github.io/static/media/CropLogo.ed6b64c9.png"
+  },
+  {
+    "revision": "b8805d0434deecaefe0dd038a1dc2b7d",
+    "url": "/alexyoung23j.github.io/static/media/Good.b8805d04.jpg"
+  },
+  {
+    "revision": "8cbe1064d58dc5e99c7b964446412251",
+    "url": "/alexyoung23j.github.io/static/media/SmartShopLogo.8cbe1064.jpg"
+  },
+  {
+    "revision": "17434cbedb6f2d25ba0a9b7a31fc6b75",
+    "url": "/alexyoung23j.github.io/static/media/groupRubik.17434cbe.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/alexyoung23j.github.io/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "52850f0dd39016c9f8af4961ff5d40a1",
+    "url": "/alexyoung23j.github.io/static/media/podcastLogo.52850f0d.jpg"
+  },
+  {
+    "revision": "2b37ba766fd39fc2a2ee2d17f5e10515",
+    "url": "/alexyoung23j.github.io/static/media/spotify.2b37ba76.png"
+  }
+]);
